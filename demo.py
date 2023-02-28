67409a669f0130ebc678ffe79218b278241f6e23
the3rdc <crumrinece@gmail.com>
2023-02-28 13:09:59 -0500

    basics

---
+++ demo.py
@@ -0,0 +1,67 @@
+import openai
+openai.api_key = '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'
+
+from llm_agent import Agent
+from llm_agent import Action
+
+#Set Up Available Functions
+fh = open("demo/business_ideas.txt", "r")
+
+def get_next_pitch():
+  next_line = fh.readline()
+  if next_line == '':
+    fh.close()
+    return "No More Pitches."
+  print(f"Evaulating pitch: {next_line.strip()}")
+  return next_line
+
+def accept(memo):
+  print(f"Accepted with memo: {memo}\n---")
+  return 'Saved'
+
+def reject(note):
+  print(f"Rejected with note: {note}\n---")
+  return 'Sent'
+
+#Create our Agent
+agent = Agent(
+  name="LOIS",
+  persona="""
+LOIS is an associate at a Venture Capital firm. 
+She spends a lot of time reviewing pitches the firm receives from a variety of sources, and can quickly tell whether or not an idea merits further investigation.
+""".strip(),
+  task="""
+LOIS's current task is to review emails with pitches the firm has received and sort them into two categories: Accepted or Rejected.
+When she rejects a pitch, she also writes a polite response to the email explaining that the firm is not interested and including a reason why. 
+When she accepts a pitch, she also writes a short memo for her manager describing why she thinks they should consider the pitch further. 
+She must do this until GET_NEXT_PITCH returns "No More Pitches".
+""".strip(),
+  actions=[
+    Action(
+      impl=get_next_pitch,
+      name="GET_NEXT_PITCH",
+      args=[],
+      returns="The text of the next pitch in the inbox, or \"No More Pitches\" if there were no pitches remaining."
+    ),
+    Action(
+      impl=reject,
+      name="REJECT",
+      args=[{
+        "name": "Response",
+        "desc": "The text pf the polite response that LOIS wrote in the rejection email."
+      }],
+      returns="Sent if the message was sent, or an error if not."
+    ),
+    Action(
+      impl=accept,
+      name="ACCEPT",
+      args=[{
+        "name": "Memo",
+        "desc": "The text of the memo LOIS wrote for her manager."
+      }],
+      returns="Saved is the memo was saved, or an error if not."
+    )
+  ]
+)
+
+print(agent.run(max_tries=30,max_log_length=4))
---
+++ llm_agent.py
@@ -0,0 +1,93 @@
+import openai
+
+class Agent:
+  def __init__(self, name, persona, task, actions):
+    self.name = name
+    self.persona = persona
+    self.task = task
+    self.actions = actions
+    self.log = ""
+
+  def prompt(self):
+    return f"""
+{self.persona}
+
+{self.task}
+
+The following is a LOG of {self.name}'s actions as they perform this task. Log entries have the following format:
+ACTION_NAME || Argument 1 || Argument N |||
+RESPONSE || Result of the action |||
+
+{self.name} is limit to the following actions:
+{self.action_list()}
+TASK_COMPLETED
+- Arg:Message: Text to return when the task has been completed
+
+--LOG--
+{self.log}
+    """.strip()
+
+  def action_list(self):
+    alist = ''
+    for action in self.actions:
+      alist += action.list_entry()
+    return alist
+
+  def get_next_action(self):
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=self.prompt(),
+      temperature=0.7,
+      max_tokens=256,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0,
+      stop=["|||"]
+    )
+    return response.choices[0].text.strip()
+  
+  def handle_action(self, action):
+    parts = action.split('||')
+    name = parts[0]
+    args = parts[1:]
+    if name == 'TASK_COMPLETED':
+      return (args[0], True)
+    for act in self.actions:
+      if act.name == name:
+        return (act.call(args), False)
+    return ("INVALID ACTION", False)
+
+  def run(self, max_tries, max_log_length):
+    for i in range(max_tries):
+      next_action = self.get_next_action()
+      (response, complete) = self.handle_action(next_action)
+      if complete:
+        return response
+      self.log += f"\n{next_action}|||\nRESPONSE||{response.strip()}|||"
+      if(len(self.log.split("\n")) > max_log_length):
+        self.log = "\n".join(self.log.split("\n")[(0 - max_log_length):])
+    
+    return "Hit max tries without a complete message."
+    
+
+
+
+class Action:
+  def __init__(self, name, args, returns, impl):
+    self.name = name
+    self.args = args
+    self.returns = returns
+    self.impl = impl
+
+  def list_entry(self):
+    entry = f"{self.name}\n"
+    for arg in self.args:
+      entry += f"- Arg:{arg['name']}: {arg['desc']}\n"
+    entry += f"- Returns:{self.returns}"
+    return entry
+
+  def call(self, args):
+    try:
+      return self.impl(*args)
+    except Exception as e:
+      return f"Error calling {self.name}: {e.message}"
